7068ab61ce5914247bf604cc66c84a24eb9c6da9
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-02-07 22:11:01 -0300

    optimization

--- Java/2020-02-07/src/Ejercicio001.java
+++ Java/2020-02-07/src/Ejercicio001.java
@@ -1,14 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Ejercicio001 {
+
+	static int pedirNumeroEntero(Scanner scanner, int limiteInferior) {
+		int numero;
+		
+		do {
+			while(!scanner.hasNextInt()) {
+				System.err.println("[!] Por favor, ingresa un número válido");
+				scanner.next();
+			}
+			
+			numero = scanner.nextInt();
+			
+			if(numero < limiteInferior) {
+				System.err.println("[!] Por favor, ingresa un número mayor o igual que "
+						+ limiteInferior + ".");
+			}
+		} while(numero < limiteInferior);
+		
+		return numero;
+	}
 	
 	public static void main(String[] args) {
-		Password contrasena = new Password(15);
-		contrasena.generarPassword();
-		System.out.println(contrasena.getPassword());
+		Scanner scanner = new Scanner(System.in);
+		List<Password> passwords = new ArrayList<Password>();
 		
-		contrasena.setLongitud(21);
-		contrasena.generarPassword();
-		System.out.println(contrasena.getPassword());
+		System.out.println("Ingrese la cantidad de contraseñas a generar");
+		int cantidadContrasenas = pedirNumeroEntero(scanner, 1);
+		
+		for(int i = 0; i < cantidadContrasenas; i++) {
+			System.out.println("Ingresa la longitud de la contraseña #" + (i + 1));
+			int longitud = pedirNumeroEntero(scanner, 1);
+			
+			Password password = new Password(longitud);
+			passwords.add(password);
+		}
+		
+		System.out.println("\nContraseña\tesFuerte()");
+		for(Password password : passwords) {
+			System.out.println(" " + password.getPassword() + "\t" + password.esFuerte());
+		}
+		
+		scanner.close();
 	}
 
 }
--- Java/2020-02-07/src/Password.java
+++ Java/2020-02-07/src/Password.java
@@ -6,92 +6,94 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Password {
-	String password;
-	int longitud;
+
+	protected String password;
+	protected int longitud;
 	
 	Password() {
-		longitud = 8;
+		this.longitud = 8;
+		generarPassword();
 	}
 	
 	Password(int longitud) {
 		this.longitud = longitud;
+		generarPassword();
 	}
 	
-	boolean esFuerte() {
-		if(longitud < 8)
+	public void generarPassword() {
+		Random random = new Random();
+		List<Character> password = new ArrayList<Character>();
+		this.password = "";
+		
+		for(int i = 0; i < this.longitud; i++) {
+			int opcion = random.nextInt(3) + 1;
+			char simbolo = 97;
+			
+			switch(opcion) {
+				case 1:
+					// Numero (0: 48, 9: 57)
+					simbolo = (char) (random.nextInt(10) + 48);
+					break;
+				case 2:
+					// Mayuscula (A: 65, Z: 90)
+					simbolo = (char) (random.nextInt(26) + 65);
+					break;
+				case 3:
+					// Minuscula (a: 97, z: 122)
+					simbolo = (char) (random.nextInt(26) + 97);
+					break;
+			}
+			
+			password.add(simbolo);
+		}
+		
+		Collections.shuffle(password);
+		for(char c : password) {
+			this.password += c;
+		}
+	}
+	
+	public boolean esFuerte() {
+		if(this.longitud < 8)
 			return false;
 		
-		Pattern pattern = Pattern.compile("([A-Z])");
+		Pattern pattern = Pattern.compile("([0-9])");
 		Matcher matcher = pattern.matcher(this.password);
+		int numeros = 0;
 		
-		int mayusculas = 0;
-		while(matcher.find())
-			mayusculas++;
+		while(matcher.find()) {
+			numeros++;
+		}
 		
 		pattern = Pattern.compile("([a-z])");
 		matcher = pattern.matcher(this.password);
-		
 		int minusculas = 0;
-		while(matcher.find())
+		
+		while(matcher.find()) {
 			minusculas++;
+		}
 		
-		pattern = Pattern.compile("([0-9])");
+		pattern = Pattern.compile("([A-Z])");
 		matcher = pattern.matcher(this.password);
+		int mayusculas = 0;
 		
-		int numeros = 0;
-		while(matcher.find())
-			numeros++;
+		while(matcher.find()) {
+			mayusculas++;
+		}
 		
-		return (mayusculas > 1 && minusculas > 0 && numeros > 4);
+		return (numeros > 4 && minusculas > 0 && mayusculas > 1);
 	}
 	
-	void generarPassword() {
-		Random random = new Random();
-		
-		if(longitud >= 8) {
-			do {
-				List<Character> password = new ArrayList<Character>();
-				this.password = "";
-				
-				for(int i = 0; i < longitud; i++) {
-					int opcion = random.nextInt(3) + 1;
-					
-					switch(opcion) {
-						case 1:
-							// Numero
-							char numero = (char) (random.nextInt(10) + 48);
-							password.add(numero);
-							break;
-						case 2:
-							// Minuscula
-							char minuscula = (char) (random.nextInt(26) + 97);
-							password.add(minuscula);
-							break;
-						case 3:
-							// Mayuscula
-							char mayuscula = (char) (random.nextInt(26) + 65);
-							password.add(mayuscula);
-							break;
-					}
-				}
-				
-				Collections.shuffle(password);
-				for(char c : password)
-					this.password += c;
-			} while (!esFuerte());
-		} else
-			System.err.println("Longitud de contrasena debe ser mayor o igual a 8");
-	}
-	
-	String getPassword() {
+	public String getPassword() {
 		return this.password;
 	}
 	
-	int getLongitud() {
+	public int getLontigud() {
 		return this.longitud;
 	}
-
-	void setLongitud(int longitud) {
+	
+	public void setLongitud(int longitud) {
 		this.longitud = longitud;
 	}
+	
 }

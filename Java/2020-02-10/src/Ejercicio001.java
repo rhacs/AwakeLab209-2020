24f14c1af1b1c96583d05f45107e13ffe30bf15f
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-02-10 16:29:01 -0300

    Replaced tabs with spaces

--- Java/2020-02-10/src/Ejercicio001.java
+++ Java/2020-02-10/src/Ejercicio001.java
@@ -4,28 +4,28 @@ import java.util.Random;
 
 public class Ejercicio001 {
 
-	public static void main(String[] args) {
-		List<Producto> productos = new ArrayList<Producto>();
-		Random random = new Random();
-		
-		System.out.println("Listado de productos:");
-		for(int i = 1; i < 11; i++) {
-			boolean perecible = random.nextBoolean();
-			
-			Producto producto = new Producto(
-					"Producto #" + i,
-					random.nextInt(10000) + 1000,
-					perecible,
-					(perecible ? (random.nextInt(10) + 1) : 0));
-			
-			productos.add(producto);
-			System.out.println("\t" + producto);
-		}
+    public static void main(String[] args) {
+        List<Producto> productos = new ArrayList<Producto>();
+        Random random = new Random();
+        
+        System.out.println("Listado de productos:");
+        for(int i = 1; i < 11; i++) {
+            boolean perecible = random.nextBoolean();
+            
+            Producto producto = new Producto(
+                    "Producto #" + i,
+                    random.nextInt(10000) + 1000,
+                    perecible,
+                    (perecible ? (random.nextInt(10) + 1) : 0));
+            
+            productos.add(producto);
+            System.out.println("\t" + producto);
+        }
 
-		System.out.println("\nPrecio final de acuerdo a caducidad:");
-		for(Producto producto : productos)
-			System.out.println("\t" + producto.getNombre() + " $" + producto.calcularPrecio(5));
-		
-	}
+        System.out.println("\nPrecio final de acuerdo a caducidad:");
+        for(Producto producto : productos)
+            System.out.println("\t" + producto.getNombre() + " $" + producto.calcularPrecio(5));
+        
+    }
 
 }
